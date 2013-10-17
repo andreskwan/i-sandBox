bc80f347dd8a530774305f672a8e6f9294aff020
ANDRES KWAN <andreskwan@gmail.com>
2013-10-17 06:16:19 -0500

    Initial Commit

---
+++ SandBox/KwNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KwNAppDelegate.h
+//  SandBox
+//
+//  Created by Andres Kwan on 10/17/13.
+//  Copyright (c) 2013 Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KwNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SandBox/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SandBox
+//
+//  Created by Andres Kwan on 10/17/13.
+//  Copyright (c) 2013 Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KwNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KwNAppDelegate class]));
+    }
+}
